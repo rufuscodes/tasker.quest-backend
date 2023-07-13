a9fcd92f272cb0e0979da3f6c20cd56d4d804f9b
Isaac Guzman <isaacdatcolt@Isaacs-MacBook-Pro.local>
2023-07-12 20:49:45 -0500

    feat: add event Model

---
+++ models/event.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    priority: {
+        type: String,
+        enum: ['High', 'Medium', 'Low'],
+        default: 'Medium',
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
