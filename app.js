33f51ae362ede82c9d4a9faa2a782988179cb1bc
rufuscodes <rufuscodes@gmail.com>
2023-07-15 10:26:24 -0700

    added: Auth routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
 require('./config/passport')(passport);
+const User = require('./models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
 // create app
 const app = express();
 
@@ -20,6 +24,107 @@ app.use('/users', require('./controllers/users'));
 app.use('/events', require('./controllers/events'));
 app.use('/tasks', require('./controllers/tasks'));
 
+app.post('/signup', (req, res) => {
+    // POST - adding the new user to the database
+    console.log('===> Inside of /signup');
+    console.log('===> /register -> req.body', req.body);
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            // if email already exists, a user will come back
+            if (user) {
+                // send a 400 response
+                return res.status(400).json({ message: 'Email already exists' });
+            } else {
+                // Create a new user
+                const newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    jobTitle: req.body.jobTitle,
+                    birthdate: new Date(),
+                    "address.streetAddress": req.body.streetAddress,
+                    "address.city": req.body.city,
+                    "address.state": req.body.state,
+                    "address.zipCode": req.body.zipCode,
+                    number: req.body.number,
+                    password: req.body.password
+                });
+
+                // Salt and hash the password - before saving the user
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) throw Error;
+
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) console.log('==> Error inside of hash', err);
+                        // Change the password in newUser to the hash
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(createdUser => {
+                                // remove password from being returned inside of response, still in DB
+                                if (createdUser.password) {
+                                    createdUser.password = '...' // hide the password
+                                    res.json({ user: createdUser });
+                                }
+                            })
+                            .catch(err => {
+                                console.log('error with creating new user', err);
+                                res.json({ message: 'Error occured... Please try again.' });
+                            });
+                    });
+                });
+            }
+        })
+        .catch(err => {
+            console.log('Error finding user', err);
+            res.json({ message: 'Error occured... Please try again.' })
+        })
+});
+
+app.post('/login', async (req, res) => {
+    // POST - finding a user and returning the user
+    console.log('===> Inside of /login');
+    console.log('===> /login -> req.body', req.body);
+
+    const foundUser = await User.findOne({ email: req.body.email });
+
+    if (foundUser) {
+        // user is in the DB
+        let isMatch = await bcrypt.compareSync(req.body.password, foundUser.password);
+        console.log('Does the passwords match?', isMatch);
+        if (isMatch) {
+            // if user match, then we want to send a JSON Web Token
+            // Create a token payload
+            // add an expiredToken = Date.now()
+            // save the user
+            const payload = {
+                id: foundUser.id,
+                email: foundUser.email,
+                firstName: foundUser.firstName,
+                lastName: foundUser.lastName,
+                address: foundUser.address,
+                birthdate: foundUser.birthdate,
+                jobTitle: foundUser.jobTitle,
+                number: foundUser.number
+            }
+
+            jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+                if (err) {
+                    res.status(400).json({ message: 'Session has endedd, please log in again' });
+                }
+                const legit = jwt.verify(token, JWT_SECRET, { expiresIn: 60 });
+                console.log('===> legit', legit);
+                delete legit.password; // remove before showing response
+                res.json({ success: true, token: `Bearer ${ token }`, userData: legit });
+            });
+
+        } else {
+            return res.status(400).json({ message: 'Email or Password is incorrect' });
+        }
+    } else {
+        return res.status(400).json({ message: 'User not found' });
+    }
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
